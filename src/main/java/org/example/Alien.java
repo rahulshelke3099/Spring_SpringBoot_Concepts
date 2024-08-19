8109ee31c312d258a768ee41e137655f6074ae27
rahulshelke3099 <rahulshelke3099@gmail.com>
2024-08-19 13:41:56 +0530

    Setter injection using reference attribute

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -4,6 +4,17 @@ public class Alien {
 
     private int age;
 
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        System.out.println("Setter called for laptop");
+        this.laptop = laptop;
+    }
+
+    private Laptop  laptop;
+
     public int getAge() {
         return age;
     }
@@ -13,10 +24,11 @@ public class Alien {
     }
 
     public Alien() {
-        System.out.println("Object Created");
+        System.out.println("Alien Object Created");
     }
 
     public void code() {
         System.out.println("Coding");
+        laptop.compile();
     }
 }

18cd671fe95057cf06848a056d5b9cfeb67013ba
rahulshelke3099 <rahulshelke3099@gmail.com>
2024-08-23 20:09:35 +0530

    Get bean by type

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,11 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
             Alien alien = (Alien) context.getBean("alien");
-            alien.getAge();
+
+//            Get bean by type
+        Alien alien1 = context.getBean("alien",Alien.class);
+
+        alien.getAge();
             System.out.println(alien.getAge());
             alien.code();
     }

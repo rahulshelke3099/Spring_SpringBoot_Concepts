cf6d8ac11aa10373e276ad71cf1fe2eb62f2c24e
rahulshelke3099 <rahulshelke3099@gmail.com>
2024-08-25 17:25:19 +0530

    Bean by Java Configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-            Alien alien = (Alien) context.getBean("alien");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//            Alien alien = (Alien) context.getBean("alien");
 
 //            Get bean by type
-        Alien alien1 = context.getBean("alien",Alien.class);
-
-        alien.getAge();
-            System.out.println(alien.getAge());
-            alien.code();
+//        Alien alien1 = context.getBean("alien",Alien.class);
+//
+//        alien.getAge();
+//            System.out.println(alien.getAge());
+//            alien.code();
     }
 }
 

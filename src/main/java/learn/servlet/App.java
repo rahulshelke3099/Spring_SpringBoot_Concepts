6563fcc1d010d110c8616b19235011e2cc71d944
rahulshelke3099 <rahulshelke3099@gmail.com>
2024-10-22 23:53:05 +0530

    Running servlet project using embedded tomcat jar

---
+++ src/main/java/learn/servlet/App.java
@@ -0,0 +1,14 @@
+package learn.servlet;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App {
+
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("Hello World !!");
+        Tomcat tomcat = new Tomcat();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}

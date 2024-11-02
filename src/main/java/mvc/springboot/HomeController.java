5ed99e2075390c98a199d78c7188dd64caf00966
rahulshelke3099 <rahulshelke3099@gmail.com>
2024-11-02 17:18:56 +0530

    Requesting mapping

--- src/main/java/mvc/springboot/HomeController.java
+++ src/main/java/mvc/springboot/HomeController.java
@@ -1,10 +1,12 @@
 package mvc.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
     public String HomePage() {
         return "index.jsp";
     }

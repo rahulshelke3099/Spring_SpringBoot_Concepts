b511cbc6634adb3d6379e40c5b6e3e2a88b07724
rahulshelke3099 <rahulshelke3099@gmail.com>
2024-11-03 13:34:21 +0530

    model object

--- src/main/java/mvc/springboot/HomeController.java
+++ src/main/java/mvc/springboot/HomeController.java
@@ -3,6 +3,7 @@ package mvc.springboot;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,12 +16,13 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1")int num1, @RequestParam("num2") int num2, HttpSession httpSession) {
+    public String add(@RequestParam("num1")int num1, @RequestParam("num2") int num2, Model model) {
 //       public String add(HttpServletRequest request, HttpSession httpSession) {
 //       int num1 =  Integer.valueOf(request.getParameter("num1"));
 //       int num2 =  Integer.valueOf(request.getParameter("num2"));
        int result = num1 + num2;
-       httpSession.setAttribute("result", result);
+//       httpSession.setAttribute("result", result);
+        model.addAttribute("result",result);
         return "result.jsp";
     }
 }
---
+++ src/main/webapp/views/result.jsp
@@ -0,0 +1,13 @@
+<%@page language="java" %>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css"/>
+</head>
+<body>
+<!--     using plain Java
+        <h2>Result is:<%= session.getAttribute("result") %></h2>  -->
+
+
+       <!--  using jstl tags -->
+        <h2>Result is: ${result}  </h2></body>
+</html>
